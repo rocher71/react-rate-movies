08c8abf92c79e5f65a2ac378790c14356dd1ec2a
Yejin Kim <qja086351@naver.com>
2022-04-06 19:58:35 +0900

    달러 코인 변환기

--- src/App.js
+++ src/App.js
@@ -3,37 +3,59 @@ import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    //returns [toDo, currentArray[0], currentArray[1], ...]
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const onSelect = (event) => {
+    setValue(0);
+    setIndex(event.target.value);
   };
-  console.log(toDos);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select value={index} onChange={onSelect}>
+          {coins.map((coin, index) => (
+            <option value={index}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      {/* api 불러오는 중에는 loaing이 뜸. */}
+      {coins.length == 0 ? null : (
+        <div>
+          <h1>{coins[index].name}</h1>
+          <div>
+            <label>USD</label>
+            <input type="number" value={value} onChange={onChange}></input>
+          </div>
+          <div>
+            <label>{coins[index].name}</label>
+            <input
+              type="number"
+              value={(value / coins[index].quotes.USD.price).toFixed(5)}
+            ></input>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
