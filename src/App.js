2f3afcde623da461c6a699d0ebf26f608929e998
Yejin Kim <qja086351@naver.com>
2022-04-06 18:12:59 +0900

    #7.1 Practice Movie App - To Do List(2)

--- src/App.js
+++ src/App.js
@@ -28,6 +28,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
