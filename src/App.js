ace1a2bca4e7c5c04b7fc070e30ee8181268a681
Yejin Kim <qja086351@naver.com>
2022-04-06 14:46:12 +0900

    #6.1 Effects - useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
+  //useEffect(func, []) -> 한번만 실행
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
