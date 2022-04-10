409e44cba07e77cbf55c7c79b6793b1849c84db7
Yejin Kim <qja086351@naver.com>
2022-04-11 00:12:29 +0900

    #7.3 Practice Movie App - Movie App(1)

--- src/App.js
+++ src/App.js
@@ -1,59 +1,38 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [index, setIndex] = useState(0);
-  const [value, setValue] = useState(0);
-
-  const onSelect = (event) => {
-    setValue(0);
-    setIndex(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select value={index} onChange={onSelect}>
-          {coins.map((coin, index) => (
-            <option value={index}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      {/* api 불러오는 중에는 loaing이 뜸. */}
-      {coins.length == 0 ? null : (
         <div>
-          <h1>{coins[index].name}</h1>
-          <div>
-            <label>USD</label>
-            <input type="number" value={value} onChange={onChange}></input>
-          </div>
-          <div>
-            <label>{coins[index].name}</label>
-            <input
-              type="number"
-              value={(value / coins[index].quotes.USD.price).toFixed(5)}
-            ></input>
-          </div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
     </div>
