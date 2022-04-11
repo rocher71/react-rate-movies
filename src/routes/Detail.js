a93099a90f5ffa7538028d040f4e5ae00b7397ce
Yejin Kim <qja086351@naver.com>
2022-04-12 02:06:24 +0900

    detail 페이지 작업
    fetch로 api 호출 후 useState를 이용해 객체에 담아줌
    제목, 별점, 언어 등 간단한 정보 뿌려줌
    링크 클릭 시 영화 소개 페이지로 연결
    
    해야할 것 :
    genres 배열 잘 보여주기, css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json.data.movie);
+    setLoading(false);
+    setMovie(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  console.log("movie : ", movie);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          <h1>{movie.title_english}</h1>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <div>
+            <ul>
+              <li>rating : {movie.rating}</li>
+              <li>runtime : {movie.runtime}</li>
+              <li>language : {movie.language}</li>
+              {/* <li>genres : {movie.genres.map((genre) => ` ${genre},`)}</li> */}
+            </ul>
+            <p>{movie.description_intro}</p>
+            <a href={movie.url} target="_blank">
+              See more &gt;{" "}
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
 
