10d387501126285b9ab6ecaab5b5796bf8124863
Yejin Kim <qja086351@naver.com>
2022-04-12 00:51:00 +0900

    #7.4 Practice Movie App - React Router
    App.js 파일에 있던 컴포넌트들을 'router' 폴더 안으로 분리.
    App.js에선 path에 맞게 component들 연결시켜줌.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        {/* url '/' 에 접속하면 <Home/> component render */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <Link to="/movie">
+        <h2>{title}</h2>
+      </Link>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
